6fb39fa05875a159c132592b3cfcd14a7ba43226
Majd Imar <majd2012995@gmail.com>
2020-02-24 10:13:20 +0200

    edited

--- Js1/app.js
+++ Js1/app.js
@@ -1,64 +1,63 @@
-var Hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var Hours = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'];
 var Seattle = {
-
+    loc1: 'Seattle',
     max: 65,
     min: 23,
     avg: 6.3,
 
-    custrandomnum: 0,
-    customer: [],
-    Total: 0,
+    rancut: 0, //random number custumer
+    amountp: [], //amount cookies per hour
+    Total: 0, //total cookies
 
     totalcookies: function () {
-        
+
 
         for (var i = 0; i < Hours.length; i++) {
-            this.custrandomnum = Math.floor(Math.random() * (this.max - this.min) + this.min );
-            this.custrandomnum = Math.floor(this.custrandomnum * this.avg);
-            this.Total = this.Total + this.custrandomnum;
-            this.customer[i] = this.custrandomnum;
+            this.rancut = Math.floor(Math.random() * (this.max - this.min) + this.min);
+            this.rancut = Math.floor(this.rancut * this.avg);
+            this.Total = this.Total + this.rancut;
+            this.amountp[i] = this.rancut;
 
         }
-      
-       console.log(this.custrandomnum);
-       console.log(this.customer);
+
+        console.log(this.rancut);
+        console.log(this.amountp);
 
         console.log(this.Total);
+    },
+
+    render: function () {
+
+    var container = document.getElementById('Finalcookies');
+    //  console.log(container) ;
+    var articleE1 = document.createElement('artilce');
+    console.log(articleE1);
+    container.appendChild(articleE1);
+    var h1E1 = document.createElement('h1');
+    articleE1.appendChild(h1E1);
+    h1E1.textContent = this.loc1;
+    var ulE1 = document.createElement('ul')
+    articleE1.appendChild(ulE1);
+    for (var y = 0; y < Hours.length ; y++) {
+        var liE1 = document.createElement('li');
+        ulE1.appendChild(liE1);
+        liE1.textContent = ` ${Hours[y]} ${this.amountp[y]} cookies `;
+
+
+
+
+
+
+
+    }
+    liE1.textContent = ` Total: ${this.Total} cookies `;
     }
 
 
 
+
 }
+
 Seattle.totalcookies();
+    Seattle.render();
 
-var Tokyo = {
-
-    max: 24,
-    min: 3,
-    avg:1.2,
-
-    custrandomnum1: 0,
-    customer1: [],
-    Total1: 0,
-
-    totalcookies1: function () {
-        
-
-        for (var i = 0; i < Hours.length; i++) {
-            this.custrandomnum1 = Math.floor(Math.random() * (this.max - this.min) + this.min );
-            this.custrandomnum1= Math.floor(this.custrandomnum1 * this.avg);
-            this.Total1 = this.Total + this.custrandomnum1;
-            this.customer1[i] = this.custrandomnum1;
-
-        }
-        
-        console.log(this.custrandomnum1);
-        console.log(this.customer1);
-
-        console.log(this.Total1);
-    }
-
-
-
-}
-Tokyo.totalcookies1();
