ebd755844860efa8d260e50a3ceeba29ad88f435
Majd Imar <majd2012995@gmail.com>
2020-02-29 14:56:30 +0200

    g

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+
+
 var Hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var locations = [];
 
@@ -98,6 +100,7 @@ myform.addEventListener('submit',function(event){
     var max=parseInt(event.target.max.value);
     var min=parseInt(event.target.min.value);
     var avg =parseFloat(event.target.avg.value);
+
     //console.log(min);
     if(min<max){
         
